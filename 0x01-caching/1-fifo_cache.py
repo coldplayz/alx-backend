4536ae642a6e1064ab4637b05de61c6323ec2ccd
greenbel <obisann@gmail.com>
2023-05-16 18:46:10 +0000

    0x01: minor adjustments

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -2,38 +2,7 @@
 """ BaseCaching module
 """
 from datetime import datetime
-
-
-class BaseCaching():
-    """ BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """ Initiliaze
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item):
-        """ Add an item in the cache
-        """
-        raise NotImplementedError(
-                "put must be implemented in your cache class")
-
-    def get(self, key):
-        """ Get an item by key
-        """
-        raise NotImplementedError(
-                "get must be implemented in your cache class")
+from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
@@ -47,7 +16,7 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key is None and item is None:
+        if key is None or item is None:
             # do nothing
             return
 
