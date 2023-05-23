12767f5e8be3918600f74f291b5688d50a870676
greenbel <obisann@gmail.com>
2023-05-23 19:57:48 +0000

    0x02-T4: init

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Basic Flask app.
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration for the app.
+    """
+    LANGUAGES = ["en", "fr"]
+
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+config = Config()
+
+app = Flask(__name__)
+app.config.from_object(config)
+babel = Babel(app)
+
+
+# define a locale selector
+@babel.localeselector
+def get_locale():
+    """Selects the most appropriate locale to use per request.
+
+    Executed in a request context.
+    """
+    # check if locale argument in query string
+    args = request.args
+    locale = args.get('locale', None)
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """Index page.
+    """
+    return render_template('3-index.html')
